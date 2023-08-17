6d29ec55b0a4ac12025d022a411ed377cbdb6420
kkatenev <kkatenev@mail.ru>
2023-08-17 15:51:31 +0700

    In process

---
+++ FiberTrade2/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace FiberTrade2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.destinationIPTextBox = new System.Windows.Forms.TextBox();
+            this.destinationPortTextBox = new System.Windows.Forms.TextBox();
+            this.sentPacketsLabel = new System.Windows.Forms.Label();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.StartListeningButton = new System.Windows.Forms.Button();
+            this.StopListeningButton = new System.Windows.Forms.Button();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.listenIPTextBox = new System.Windows.Forms.TextBox();
+            this.listenPortTextBox = new System.Windows.Forms.TextBox();
+            this.receivedPacketsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // destinationIPTextBox
+            // 
+            this.destinationIPTextBox.Location = new System.Drawing.Point(146, 103);
+            this.destinationIPTextBox.Name = "destinationIPTextBox";
+            this.destinationIPTextBox.Size = new System.Drawing.Size(100, 20);
+            this.destinationIPTextBox.TabIndex = 0;
+            this.destinationIPTextBox.Text = "IP";
+            // 
+            // destinationPortTextBox
+            // 
+            this.destinationPortTextBox.Location = new System.Drawing.Point(252, 103);
+            this.destinationPortTextBox.Name = "destinationPortTextBox";
+            this.destinationPortTextBox.Size = new System.Drawing.Size(100, 20);
+            this.destinationPortTextBox.TabIndex = 1;
+            this.destinationPortTextBox.Text = "PORT";
+            // 
+            // sentPacketsLabel
+            // 
+            this.sentPacketsLabel.AutoSize = true;
+            this.sentPacketsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sentPacketsLabel.Location = new System.Drawing.Point(141, 57);
+            this.sentPacketsLabel.Name = "sentPacketsLabel";
+            this.sentPacketsLabel.Size = new System.Drawing.Size(128, 25);
+            this.sentPacketsLabel.TabIndex = 2;
+            this.sentPacketsLabel.Text = "Sent Packets";
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(146, 259);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(75, 23);
+            this.SendButton.TabIndex = 3;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // StartListeningButton
+            // 
+            this.StartListeningButton.Location = new System.Drawing.Point(564, 241);
+            this.StartListeningButton.Name = "StartListeningButton";
+            this.StartListeningButton.Size = new System.Drawing.Size(87, 41);
+            this.StartListeningButton.TabIndex = 4;
+            this.StartListeningButton.Text = "StartListening";
+            this.StartListeningButton.UseVisualStyleBackColor = true;
+            this.StartListeningButton.Click += new System.EventHandler(this.StartListeningButton_Click);
+            // 
+            // StopListeningButton
+            // 
+            this.StopListeningButton.Location = new System.Drawing.Point(564, 295);
+            this.StopListeningButton.Name = "StopListeningButton";
+            this.StopListeningButton.Size = new System.Drawing.Size(87, 41);
+            this.StopListeningButton.TabIndex = 5;
+            this.StopListeningButton.Text = "StopListening";
+            this.StopListeningButton.UseVisualStyleBackColor = true;
+            this.StopListeningButton.Click += new System.EventHandler(this.StopListeningButton_Click);
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Location = new System.Drawing.Point(146, 223);
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(100, 20);
+            this.messageTextBox.TabIndex = 6;
+            this.messageTextBox.Text = "message";
+            // 
+            // listenIPTextBox
+            // 
+            this.listenIPTextBox.Location = new System.Drawing.Point(497, 103);
+            this.listenIPTextBox.Name = "listenIPTextBox";
+            this.listenIPTextBox.Size = new System.Drawing.Size(100, 20);
+            this.listenIPTextBox.TabIndex = 7;
+            this.listenIPTextBox.Text = "IP";
+            // 
+            // listenPortTextBox
+            // 
+            this.listenPortTextBox.Location = new System.Drawing.Point(613, 102);
+            this.listenPortTextBox.Name = "listenPortTextBox";
+            this.listenPortTextBox.Size = new System.Drawing.Size(100, 20);
+            this.listenPortTextBox.TabIndex = 8;
+            this.listenPortTextBox.Text = "PORT";
+            // 
+            // receivedPacketsLabel
+            // 
+            this.receivedPacketsLabel.AutoSize = true;
+            this.receivedPacketsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.receivedPacketsLabel.Location = new System.Drawing.Point(492, 57);
+            this.receivedPacketsLabel.Name = "receivedPacketsLabel";
+            this.receivedPacketsLabel.Size = new System.Drawing.Size(168, 25);
+            this.receivedPacketsLabel.TabIndex = 9;
+            this.receivedPacketsLabel.Text = "Received Packets";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.receivedPacketsLabel);
+            this.Controls.Add(this.listenPortTextBox);
+            this.Controls.Add(this.listenIPTextBox);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.StopListeningButton);
+            this.Controls.Add(this.StartListeningButton);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.sentPacketsLabel);
+            this.Controls.Add(this.destinationPortTextBox);
+            this.Controls.Add(this.destinationIPTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox destinationIPTextBox;
+        private System.Windows.Forms.TextBox destinationPortTextBox;
+        private System.Windows.Forms.Label sentPacketsLabel;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.Button StartListeningButton;
+        private System.Windows.Forms.Button StopListeningButton;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.TextBox listenIPTextBox;
+        private System.Windows.Forms.TextBox listenPortTextBox;
+        private System.Windows.Forms.Label receivedPacketsLabel;
+    }
+}
+
---
+++ FiberTrade2/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FiberTrade2
+{
+    public partial class Form1 : Form
+    {
+        private UdpClient udpClient;
+        private int sentPacketsCount;
+        private int receivedPacketsCount;
+        private int lostPacketsCount;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            udpClient = new UdpClient();
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            if (IPAddress.TryParse(destinationIPTextBox.Text, out IPAddress destinationIP) && int.TryParse(destinationPortTextBox.Text, out int destinationPort))
+            {
+                byte[] data = Encoding.ASCII.GetBytes(messageTextBox.Text);
+                udpClient.Send(data, data.Length, new IPEndPoint(destinationIP, destinationPort));
+                sentPacketsCount++;
+                sentPacketsLabel.Text = $"Sent Packets: {sentPacketsCount}";
+            }
+            else
+            {
+                MessageBox.Show("Invalid IP address or port.");
+            }
+        }
+
+        private void StartListeningButton_Click(object sender, EventArgs e)
+        {
+            if (IPAddress.TryParse(listenIPTextBox.Text, out IPAddress listenIP) && int.TryParse(listenPortTextBox.Text, out int listenPort))
+            {
+                UdpClient listener = new UdpClient(listenPort);
+                IPEndPoint endPoint = new IPEndPoint(listenIP, listenPort);
+
+                try
+                {
+                    while (true)
+                    {
+                        byte[] receivedData = listener.Receive(ref endPoint);
+                        string receivedMessage = Encoding.ASCII.GetString(receivedData);
+                        receivedPacketsCount++;
+                        receivedPacketsLabel.Text = $"Received Packets: {receivedPacketsCount}";
+                    }
+                }
+                catch (SocketException ex)
+                {
+                    // SocketException will occur when the listener is closed
+                    listener.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid IP address or port.");
+            }
+        }
+
+        private void StopListeningButton_Click(object sender, EventArgs e)
+        {
+            // Closing the listener will break out of the Receive loop
+            udpClient.Close();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            udpClient.Close();
+        }
+
+
+    }
+}
